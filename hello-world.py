f7d720e5fcfb2148360a3ba89971443bf57c6267
mauricio-mj-ucsc-alt <mauricio.r.moreno.jones@gmail.com>
2025-09-30 13:11:16 -0700

    Add files via upload
    hello world file

---
+++ hello-world.py
@@ -0,0 +1,10 @@
+#tells computer to print hello world
+def PrintHelloWorld():
+    print("hello\nworld")
+#defines main func
+def main():
+    PrintHelloWorld()
+#run the program and execute
+if __name__=="__main__":
+    main()
+
