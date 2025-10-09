ed989b70dd3bca17501d933f386778a5bbce5b6c
mauricio-mj-ucsc-alt <mauricio.r.moreno.jones@gmail.com>
2025-10-08 19:45:18 -0700

    Astr code journal prompt 2

---
+++ astrprompt2.py
@@ -0,0 +1,15 @@
+#Write a Python that prints the sum of two floating point numbers, 
+# the difference between two integers, and the product of a floating point 
+# number and an integer. In each case, have the program print out the data type of the resulting answer.
+a = 1.5 + 1.5
+print(a)
+print(type(a))
+
+b = 1-1
+print(b)
+print(type(b))
+
+c = 1.5 ** 1
+print(c)
+print(type(c))
+
